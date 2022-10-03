26ba3759dce054c130e86f2c262837e86323eb25
brijeshmaster <brijeshkumarmaisuriya@gmail.com>
2022-10-03 21:38:24 +0200

    createdatebase

---
+++ config/database.js
@@ -0,0 +1,27 @@
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "brij"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Galaxy222"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
